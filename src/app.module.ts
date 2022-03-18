337fe82591b20d5eff50f7aae70eab9600c227f3
wayn3r <w9delacruz@gmail.com>
2022-03-18 17:54:48 -0400

    add db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { TwitsModule } from './modules/twits/twits.module'
 
 @Module({
-    imports: [TwitsModule],
+    imports: [
+        TwitsModule,
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'admin',
+            password: 'secretpassword',
+            database: 'postgres',
+            autoLoadEntities: true,
+            synchronize: true,
+        }),
+    ],
 })
 export class AppModule {}
